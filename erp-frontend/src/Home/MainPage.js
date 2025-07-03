a3c7163783f82c5b7056ba359216ce9875f203d0
Aman Raj <billygates124356@gmail.com>
2025-07-03 20:50:22 +0530

    Added home page, added routing

---
+++ erp-frontend/src/Home/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const MainHomePage = () => {
+
+    return(
+        <div>
+            <p>Mian home page</p>
+        </div>
+    )
+}
+
+export default MainHomePage

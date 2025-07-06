a15108ee409dacead9b3b3abd391dce1a26689b6
Aman Raj <billygates124356@gmail.com>
2025-07-06 09:32:20 +0530

    Added JWT, starting backend. Routes not implemented

---
+++ erp-backend/utils/jwt.py
@@ -0,0 +1,43 @@
+
+from jose import jwt, JWTError
+
+from passlib.context import CryptContext
+
+from datetime import datetime, timedelta, timezone
+
+# Configuration for JWT
+SECRET_KEY = ""
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+COOKIE_NAME = "access_token"
+
+# Now create a method to hash the password 
+pwd_context = CryptContext(schemes = ["bcrypt"],
+                           depreciation = "auto")
+
+
+def generate_jwt_token(data : dict, minutes_to_expire : float = None):
+    """Utility function to generate JWT tokens for authentication
+
+    Args:
+        data (dict): The data to be hashed as JWT token 
+        minutes_to_expire (float): The time in minutes to be expired. Defaults to 30 minutes as standard.
+    """
+    
+    data_copy = data.copy()
+    expiry_time = datetime.now(timezone.utc) 
+    
+    if minutes_to_expire is None: 
+        expires_time += timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    else: 
+        expires_time += timedelta(minutes = minutes_to_expire)
+        
+    data_copy.update({
+        "expiration" : expires_time
+    })
+    
+    return jwt.encode(
+        data_copy, 
+        key = SECRET_KEY, 
+        algorithm = ALGORITHM
+    )

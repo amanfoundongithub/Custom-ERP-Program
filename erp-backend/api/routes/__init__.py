07eaed8baaf5bb6a00a81afefc92c2c7dd2dfa64
Aman Raj <billygates124356@gmail.com>
2025-07-13 20:58:24 +0530

    Fixes

--- erp-backend/api/routes/__init__.py
+++ erp-backend/api/routes/__init__.py
@@ -4,3 +4,4 @@ from api.routes.jwtHandler  import router as jwt_router
 from api.routes.otpHandler  import router as otp_router 
 from api.routes.userHandler import router as user_router
 from api.routes.companyHandler import router as company_router
+from api.routes.journalHandler import router as journal_router
